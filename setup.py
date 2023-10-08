0cbd7d0015b40708f8c341baa78e8d5c75cb20aa
Juho Kim <juho-kim@outlook.com>
2023-10-07 23:42:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 
 from setuptools import find_packages, setup
 
-with open('README.rst', 'r') as file:
-    long_description = file.read()
-
 setup(
     name='adc78h89',
     version='0.0.0.dev5',
-    description='A Python driver for Texas Instruments ADC78H89 7-Channel, 500 KSPS, 12-Bit A/D Converter',
-    long_description=long_description,
+    description='A Python driver for Texas Instruments ADC78H89 7-Channel, '
+                '500 KSPS, 12-Bit A/D Converter',
+    long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
     url='https://github.com/blueskysolarracing/adc78h89',
     author='Blue Sky Solar Racing',
@@ -29,18 +27,14 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
     ],
-    keywords=[
-        'python',
-    ],
+    keywords=['python', 'adc78h89', 'ti', 'texas instruments'],
     project_urls={
         'Documentation': 'https://adc78h89.readthedocs.io/en/latest/',
         'Source': 'https://github.com/blueskysolarracing/adc78h89',
         'Tracker': 'https://github.com/blueskysolarracing/adc78h89/issues',
     },
     packages=find_packages(),
-    install_requires=[
-         'python-periphery>=2.4.1,<3',
-    ],
+    install_requires=['python-periphery>=2.4.0,<3'],
     python_requires='>=3.11',
     package_data={'adc78h89': ['py.typed']},
 )
